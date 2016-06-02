634b73ab73e5378815f5a537c517963fb376f6fa
blakjak619 <blakjak619@gmail.com>
2016-06-02 18:38:05 +0000

    Add files via upload

---
+++ scripts-repository/Endpoint/ADCheckDW.ps1
@@ -0,0 +1,20 @@
+﻿ cls
+
+ #$username = 'DEV_SVC_DWEmlEvtMgr'
+ #$password = 'EX!JHKTYkQcB'
+
+ $username = 'sa_nav-jobqueue'
+ $password = 'leo75*zodiac'
+
+ # Get current domain using logged-on user's credentials
+ $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+ $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
+
+if ($domain.name -eq $null)
+{
+ write-host "FAIL"
+}
+else
+{
+ write-host "SUCCESS"
+}
