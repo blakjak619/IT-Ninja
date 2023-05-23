fd50dff867199873640bc3614d34b6efc6583823
blakjak619 <blakjak619@gmail.com>
2023-05-23 08:51:58 -0700

    Create ActivatePIMRoles.ps1

---
+++ scripts-repository/SpaceCadets/ActivatePIMRoles.ps1
@@ -0,0 +1,8 @@
+﻿# Get token for MS Graph by prompting for MFA
+$MsResponse = Get-MSALToken -Scopes @("https://graph.microsoft.com/.default") -ClientId "Insert Your ClientID" -RedirectUri "urn:ietf:wg:oauth:2.0:oob" -Authority "https://login.microsoftonline.com/common" -Interactive -ExtraQueryParameters @{claims='{"access_token" : {"amr": { "values": ["mfa"] }}}'}
+
+# Get token for AAD Graph
+$AadResponse = Get-MSALToken -Scopes @("https://graph.windows.net/.default") -ClientId "Insert Your ClientID" -RedirectUri "urn:ietf:wg:oauth:2.0:oob" -Authority "https://login.microsoftonline.com/common"
+
+Connect-AzureAD -AadAccessToken $AadResponse.AccessToken -MsAccessToken $MsResponse.AccessToken -AccountId: "upn" -tenantId: "tenantId"
+Enable-DCAzureADPIMRole -RolesToActivate 'Intune Administrator', 'Authentication Administrator', 'Office Apps Administrator', 'Exchange Administrator', 'Helpdesk Administrator', 'Authentication Policy Administrator', 'Teams Administrator', 'License Administrator', 'Groups Administrator', 'SharePoint Administrator', 'Conditional Access Administrator' -UseMaximumTimeAllowed -Reason 'Working tickets.'
